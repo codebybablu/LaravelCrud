ff99aa7d525a38c4f5b42652bd95ace7fc0dcf14
codebybablu <bablu.mailaxy@gmail.com>
2024-08-21 21:29:23 +0530

    third Commit

--- resources/views/Student/create.blade.php
+++ resources/views/Student/create.blade.php
@@ -6,17 +6,19 @@
     <div>
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required>
-    </div>
+    </div><br><br>
 
     <div>
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
-    </div>
+    </div><br><br>
 
     <div>
         <label for="rollno">Address</label>
         <input type="text" id="address" name="address" required>
-    </div>
+    </div><br><br>
 
     <button type="submit">Create Student</button>
+    
 </form>
+<a href="{{ route('index') }}">Student  List</a>
