ea3688c1460e0a6e409cfe865a23f63c210de754
codebybablu <bablu.mailaxy@gmail.com>
2024-09-02 13:08:21 +0530

    I use bootstrap to looks good view data which is showing in view file

--- resources/views/Student/index.blade.php
+++ resources/views/Student/index.blade.php
@@ -4,38 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <h1> Student Data </h1>
-    <a href="{{ route('student.create') }}">Create Student</a>
-    <table>
-        <thead>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Action</th>
-        </thead>
-        <tbody>
-            @php
-                $i = 1;
-            @endphp
-            @foreach ($students as $user)
+    <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <h1>Student Data</h1>
+              <a href="{{ route('student.create') }}" class="btn btn-primary btn-sm">Create Student</a>
+            </div>
+            <table class="table table-striped table-bordered">
+              <thead>
                 <tr>
-                    <td>{{ $i }}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->address }}</td>
-                    <td><a href="{{ url('student/edit' .$user->id) }}">Edit</a> &nbsp;&nbsp;  
-                        <a onclick="return confirm('Do you want to delete or not?')" href="{{ url('student/delete/' .$user->id) }}">Delete</a></td>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @php
+                  $i = 1;
+                @endphp
+                @foreach ($students as $user)
+                <tr>
+                  <th scope="row">{{ $i }}</th>
+                  <td>{{ $user->name }}</td>
+                  <td>{{ $user->email }}</td>
+                  <td>{{ $user->address }}</td>
+                  <td>
+                    <a href="{{ url('student/edit/' .$user->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                    <a onclick="return confirm('Do you want to delete or not?')" href="{{ url('student/delete/' .$user->id) }}" class="btn btn-sm btn-danger">Delete</a>
+                  </td>
                 </tr>
                 @php
-                    $i++;
+                  $i++;
                 @endphp
-            @endforeach
-        </tbody>
-    </table>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
 
 </body>
 </html>
