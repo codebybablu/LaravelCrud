4fe214a6acf83aff979d169ba9c888b2b622a2d3
codebybablu <bablu.mailaxy@gmail.com>
2024-09-03 16:27:53 +0530

    use bootstrap to looks good this create form

--- resources/views/Student/create.blade.php
+++ resources/views/Student/create.blade.php
@@ -1,24 +1,45 @@
-<h1>Create Student</h1>
-
-<form action="{{ route('student.store') }}" method="post">
-    @csrf
-
-    <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-    </div><br><br>
-
-    <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-    </div><br><br>
-
-    <div>
-        <label for="rollno">Address</label>
-        <input type="text" id="address" name="address" required>
-    </div><br><br>
-
-    <button type="submit">Create Student</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
     
-</form>
-<a href="{{ route('index') }}">Student  List</a>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h1>Create Student</h1>
+              </div>
+              <div class="card-body">
+                <form action="{{ route('student.store') }}" method="post">
+                  @csrf
+                  <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" class="form-control" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="address">Address:</label>
+                    <input type="text" id="address" name="address" class="form-control" required>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Create Student</button>
+                </form>
+              </div>
+            </div>
+            <a href="{{ route('index') }}" class="btn btn-warning">Student List</a>
+          </div>
+        </div>
+      </div>
+
+</body>
+</html>
